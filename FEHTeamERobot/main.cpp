0e6027ad21dee774cfb8157b9983588a2b5390c7
Lauren Ferguson <lauferguson6@gmail.com>
2023-02-10 09:24:23 -0500

    Fixed File

--- FEHTeamERobot/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <FEHLCD.h>
-#include "Functions/test.h"
-
-int main(void)
-{
-    LCD.Clear(BLACK);
-    LCD.WriteLine("Hello, World!");
-	return 0;
-}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <FEHLCD.h>
+#include "Functions/test.h"
+
+int main(void)
+{
+    LCD.Clear(BLACK);
+    LCD.WriteLine("Hello, World!");
+	return 0;
+}
